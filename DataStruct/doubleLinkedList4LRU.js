1f31bdd9d6086dc52d0c3662b06c2d6fb9f16022
Jellyfish <1129088994@qq.com>
2016-06-23 17:58:33 +0800

    add cache update strategy refs from win10

--- DataStruct/doubleLinkedList4LRU.js
+++ DataStruct/doubleLinkedList4LRU.js
@@ -3,3 +3,4 @@
 // http://blog.csdn.net/beiyeqingteng/article/details/7010411
 // http://www.source-code.biz/snippets/java/6.htm
 // http://www.cnblogs.com/dolphin0520/p/3749259.html
+// http://blog.jobbole.com/30940/
